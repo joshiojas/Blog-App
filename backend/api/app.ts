0f8d5f21e71fd1c3ea85aea255f4b3861349b689
joshiojas <80479115+joshiojas@users.noreply.github.com>
2024-05-15 00:44:48 +0530

    edited vercel.json

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -3,7 +3,7 @@ import Fastify, {
   type RouteShorthandOptions,
 } from "fastify";
 import { Server, IncomingMessage, ServerResponse } from "http";
-import type { Post, User } from "../types";
+import type { Post, User } from "../types.ts";
 import {
   authenticateUser,
   createPost,
@@ -14,7 +14,7 @@ import {
   getPostById,
   getPostsByAuthorId,
   hashPassword,
-} from "../lib";
+} from "../lib.ts";
 
 export default async function (server: FastifyInstance, opts: any) {
   server.get("/ping", async (request, reply) => {
